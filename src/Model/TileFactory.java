7cecd5a68f3ab91d34711713391fc4ed137156d7
Matthias Sami <matthias.sami@etu.unistra.fr>
2024-11-29 09:56:26 +0100

    Implémentation de pushTile

--- src/Model/TileFactory.java
+++ src/Model/TileFactory.java
@@ -3,14 +3,14 @@ package Model;
 public class TileFactory {
     public TileFactory(){}
     public Tile createTileAngle(){
-        return new TileAngle();
+        return new TileAngle(true);
     }
 
     public Tile createTileLine(){
-        return new TileLine();
+        return new TileLine(true);
     }
 
     public Tile createTileT(){
-        return new TileT();
+        return new TileT(true);
     }
 }
--- src/Model/TileT.java
+++ src/Model/TileT.java
@@ -1,7 +1,9 @@
 package Model;
 
 public class TileT extends Tile{
-    public TileT(){}
+    public TileT(boolean isMovable){
+        super(isMovable, false,true,true,true);
+    }
 
     @Override
     public TypeTile getType(){
@@ -9,7 +11,48 @@ public class TileT extends Tile{
     }
 
     @Override
+    public void rotate(Direction direction){
+        switch (direction){
+            case TOP:
+                setOpenTop(false);
+                setOpenRight(true);
+                setOpenBottom(true);
+                setOpenLeft(true);
+                break;
+            case RIGHT:
+                setOpenTop(true);
+                setOpenRight(false);
+                setOpenBottom(true);
+                setOpenLeft(true);
+                break;
+            case BOTTOM:
+                setOpenTop(true);
+                setOpenRight(true);
+                setOpenBottom(false);
+                setOpenLeft(true);
+                break;
+            case LEFT:
+                setOpenTop(true);
+                setOpenRight(true);
+                setOpenBottom(true);
+                setOpenLeft(false);
+                break;
+        }
+    }
+
+    @Override
     public String affichage(){
-        return "T";
+        String affichage="[T";
+        if(getIsOpenTop()){affichage+="O";}
+        else{affichage+="X";}
+        if(getIsOpenRight()){affichage+="O";}
+        else{affichage+="X";}
+        if(getIsOpenBottom()){affichage+="O";}
+        else{affichage+="X";}
+        if(getIsOpenLeft()){affichage+="O";}
+        else{affichage+="X";}
+        affichage+="]";
+
+        return affichage;
     }
 }
